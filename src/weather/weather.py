0baa5a104eaf301df7d4b3cd1c0303dd56b5fcef
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:54:15 +0100

    REFACTOR test_week_pressure_forecast_by_city_name_bad_type_exception_when_tuple_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -84,4 +84,11 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_pressure_forecast_by_city_name(self, city_name):
-        raise TypeError("Wrong type!")
+        try:
+            weather = self.data.get_week_weather_by_city_name(city_name)
+            pressure = []
+            for day in weather["list"]:
+                pressure.append(day["pressure"])
+            return pressure
+        except TypeError:
+            raise TypeError("Wrong type!")
