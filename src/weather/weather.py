aeaace1437e0289d6a844252032ca4f448d87d4a
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:39:37 +0100

    REFACTOR test_week_temperature_forecast_by_city_name_bad_value_exception_when_invalid_city_name_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -60,4 +60,13 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_temperature_forecast_by_city_name(self, city_name):
-        raise ValueError("Wrong value!")
+        try:
+            weather = self.data.get_week_weather_by_city_name(city_name)
+            temperature = []
+            for day in weather["list"]:
+                temperature.append(round(day["temp"]["day"] - 273.15, 2))
+            return temperature
+        except TypeError:
+            raise TypeError("Wrong type!")
+        except ValueError:
+            raise ValueError("Wrong value!")
