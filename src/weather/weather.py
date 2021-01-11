7288d4f2dcf7232a5a78f349efc3de6fc0c8b682
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:34:39 +0100

    REFACTOR test_week_average_pressure_by_city_name_bad_type_exception_when_bytes_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -156,4 +156,11 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_average_pressure_by_city_name(self, city_name):
-        raise TypeError("Wrong type!")
+        try:
+            weather = self.data.get_week_weather_by_city_name(city_name)
+            pressure = []
+            for day in weather["list"]:
+                pressure.append(day["pressure"])
+            return int(round(sum(pressure) / len(pressure), 0))
+        except TypeError:
+            raise TypeError("Wrong type!")
