20943143db14ede123ad89d8ad5eb22638306857
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:30:22 +0100

    REFACTOR test_current_humidity_by_city_id_bad_value_exception_when_invalid_city_id_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -51,4 +51,10 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def current_humidity_by_city_id(self, city_id):
-        raise ValueError("Wrong value!")
+        try:
+            weather = self.data.get_current_weather_by_city_id(city_id)
+            return weather["main"]["humidity"]
+        except TypeError:
+            raise TypeError("Wrong type!")
+        except ValueError:
+            raise ValueError("Wrong value!")
