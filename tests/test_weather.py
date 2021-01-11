75d128da08bed9b298f3c2c13b51b52febbdaac3
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:04:44 +0100

    RED test_week_humidity_forecast_by_city_name_Mountain_View_list_equal_to_humidities

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -663,6 +663,11 @@ class WeatherTest(unittest.TestCase):
         self.assertListEqual(self.weather.week_humidity_forecast_by_city_name(self.city_name_London),
                              [75, 95, 95, 0, 0, 0, 0])
 
+    def test_week_humidity_forecast_by_city_name_Mountain_View_list_equal_to_humidities(self):
+        self.weather.data.get_week_weather_by_city_name = MagicMock(return_value=self.week_Mountain_View_json)
+        self.assertListEqual(self.weather.week_humidity_forecast_by_city_name(self.city_name_Mountain_View),
+                             [52, 50, 84, 0, 0, 0, 0])
+
     def tearDown(self):
         self.weather = None
 
