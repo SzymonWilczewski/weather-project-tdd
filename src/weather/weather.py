76de9e2a736977ec20d76516be181ca6aabded2d
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:43:43 +0100

    REFACTOR test_week_average_humidity_by_city_name_London_less_or_equal_to_38

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -180,4 +180,8 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_average_humidity_by_city_name(self, city_name):
-        return 38
+        weather = self.data.get_week_weather_by_city_name(city_name)
+        humidity = []
+        for day in weather["list"]:
+            humidity.append(day["humidity"])
+        return int(round(sum(humidity) / len(humidity), 0))
