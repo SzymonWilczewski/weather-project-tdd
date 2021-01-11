390d4ac13f9e50905cd809688b4b35696a7ac4b2
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:53:45 +0100

    REFACTOR test_week_average_humidity_by_city_id_bad_type_exception_when_dict_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -192,4 +192,11 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_average_humidity_by_city_id(self, city_id):
-        raise TypeError("Wrong type!")
+        try:
+            weather = self.data.get_week_weather_by_city_id(city_id)
+            humidity = []
+            for day in weather["list"]:
+                humidity.append(day["humidity"])
+            return int(round(sum(humidity) / len(humidity), 0))
+        except TypeError:
+            raise TypeError("Wrong type!")
