42a4ba2fd3d24acb6c029eeef3532aa602e439a1
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 17:44:39 +0100

    REFACTOR test_current_temperature_by_city_name_London_almost_equal_7_17

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -6,4 +6,5 @@ class Weather:
         self.data = WeatherData()
 
     def current_temperature_by_city_name(self, city_name):
-        return 7.17
+        weather = self.data.get_current_weather_by_city_name(city_name)
+        return round(weather["main"]["temp"] - 273.15, 2)
