88f60d93e14d1b058fcc2a29a1697f8d2ac71e70
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:07:11 +0100

    RED test_week_humidity_forecast_by_city_name_bad_value_exception_when_invalid_city_name_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -108,11 +108,4 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_humidity_forecast_by_city_name(self, city_name):
-        try:
-            weather = self.data.get_week_weather_by_city_name(city_name)
-            humidity = []
-            for day in weather["list"]:
-                humidity.append(day["humidity"])
-            return humidity
-        except TypeError:
-            raise TypeError("Wrong type!")
+        pass
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -673,6 +673,11 @@ class WeatherTest(unittest.TestCase):
         with self.assertRaisesRegex(TypeError, "Wrong type!"):
             self.weather.week_humidity_forecast_by_city_name(range(1))
 
+    def test_week_humidity_forecast_by_city_name_bad_value_exception_when_invalid_city_name_given(self):
+        self.weather.data.get_week_weather_by_city_name = MagicMock(side_effect=ValueError("Wrong value!"))
+        with self.assertRaisesRegex(ValueError, "Wrong value!"):
+            self.weather.week_humidity_forecast_by_city_name("xyz")
+
     def tearDown(self):
         self.weather = None
 
