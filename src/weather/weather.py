4e3dc6198172be8400e4ec3361bd0bb085608412
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:17:21 +0100

    REFACTOR test_current_pressure_by_city_id_bad_value_exception_when_invalid_city_id_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -33,4 +33,10 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def current_pressure_by_city_id(self, city_id):
-        raise ValueError("Wrong value!")
+        try:
+            weather = self.data.get_current_weather_by_city_id(city_id)
+            return weather["main"]["pressure"]
+        except TypeError:
+            raise TypeError("Wrong type!")
+        except ValueError:
+            raise ValueError("Wrong value!")
