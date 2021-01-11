05094d7308ea203fbd1ce73c34f54e6659b4ed1d
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:32:24 +0100

    RED test_week_temperature_forecast_by_city_name_London_13_52_in_temperatures

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -58,3 +58,6 @@ class Weather:
             raise TypeError("Wrong type!")
         except ValueError:
             raise ValueError("Wrong value!")
+
+    def week_temperature_forecast_by_city_name(self, city_name):
+        pass
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -95,6 +95,199 @@ class WeatherTest(unittest.TestCase):
             "name": "Mountain View",
             "cod": 200
         }
+        self.week_London_json = {
+            "city": {
+                "id": 2643743,
+                "name": "London",
+                "coord": {
+                    "lon": -0.1258,
+                    "lat": 51.5085
+                },
+                "country": "GB",
+                "population": 0,
+                "timezone": 3600
+            },
+            "cod": "200",
+            "message": 0.7809187,
+            "cnt": 7,
+            "list": [
+                {
+                    "dt": 1487242800,
+                    "temp": {
+                        "day": 286.67,
+                        "min": 272.78,
+                        "max": 286.67,
+                        "night": 273.34,
+                        "eve": 277.05,
+                        "morn": 281.56
+                    },
+                    "pressure": 972.73,
+                    "humidity": 75,
+                    "weather": [
+                        {
+                            "id": 800,
+                            "main": "Clear",
+                            "description": "sky is clear",
+                            "icon": "01d"
+                        }
+                    ],
+                    "speed": 1.81,
+                    "deg": 248,
+                    "clouds": 0
+                },
+                {
+                    "dt": 1487329200,
+                    "temp": {
+                        "day": 278.25,
+                        "min": 275.04,
+                        "max": 278.25,
+                        "night": 275.04,
+                        "eve": 275.64,
+                        "morn": 276.48
+                    },
+                    "pressure": 966.98,
+                    "humidity": 95,
+                    "weather": [
+                        {
+                            "id": 600,
+                            "main": "Snow",
+                            "description": "light snow",
+                            "icon": "13d"
+                        }
+                    ],
+                    "speed": 3.17,
+                    "deg": 262,
+                    "clouds": 92,
+                    "rain": 11.74,
+                    "snow": 0.31
+                },
+                {
+                    "dt": 1487415600,
+                    "temp": {
+                        "day": 277.93,
+                        "min": 269.55,
+                        "max": 278.37,
+                        "night": 269.55,
+                        "eve": 273.8,
+                        "morn": 274.56
+                    },
+                    "pressure": 966.06,
+                    "humidity": 95,
+                    "weather": [
+                        {
+                            "id": 600,
+                            "main": "Snow",
+                            "description": "light snow",
+                            "icon": "13d"
+                        }
+                    ],
+                    "speed": 0.86,
+                    "deg": 244,
+                    "clouds": 8,
+                    "snow": 0.09
+                },
+                {
+                    "dt": 1487502000,
+                    "temp": {
+                        "day": 276.41,
+                        "min": 267.97,
+                        "max": 276.41,
+                        "night": 269.77,
+                        "eve": 273.57,
+                        "morn": 267.97
+                    },
+                    "pressure": 933.27,
+                    "humidity": 0,
+                    "weather": [
+                        {
+                            "id": 800,
+                            "main": "Clear",
+                            "description": "sky is clear",
+                            "icon": "01d"
+                        }
+                    ],
+                    "speed": 1.57,
+                    "deg": 142,
+                    "clouds": 74
+                },
+                {
+                    "dt": 1487588400,
+                    "temp": {
+                        "day": 276.28,
+                        "min": 271.12,
+                        "max": 276.28,
+                        "night": 273.12,
+                        "eve": 274.52,
+                        "morn": 271.12
+                    },
+                    "pressure": 938.21,
+                    "humidity": 0,
+                    "weather": [
+                        {
+                            "id": 600,
+                            "main": "Snow",
+                            "description": "light snow",
+                            "icon": "13d"
+                        }
+                    ],
+                    "speed": 1.79,
+                    "deg": 248,
+                    "clouds": 88,
+                    "rain": 0.93,
+                    "snow": 0.38
+                },
+                {
+                    "dt": 1487674800,
+                    "temp": {
+                        "day": 278.1,
+                        "min": 271.73,
+                        "max": 278.1,
+                        "night": 272.55,
+                        "eve": 274.01,
+                        "morn": 271.73
+                    },
+                    "pressure": 945.82,
+                    "humidity": 0,
+                    "weather": [
+                        {
+                            "id": 800,
+                            "main": "Clear",
+                            "description": "sky is clear",
+                            "icon": "01d"
+                        }
+                    ],
+                    "speed": 2.19,
+                    "deg": 262,
+                    "clouds": 25,
+                    "snow": 0.01
+                },
+                {
+                    "dt": 1487761200,
+                    "temp": {
+                        "day": 281.76,
+                        "min": 273.21,
+                        "max": 281.76,
+                        "night": 278.88,
+                        "eve": 279.22,
+                        "morn": 273.21
+                    },
+                    "pressure": 945.21,
+                    "humidity": 0,
+                    "weather": [
+                        {
+                            "id": 500,
+                            "main": "Rain",
+                            "description": "light rain",
+                            "icon": "10d"
+                        }
+                    ],
+                    "speed": 2.98,
+                    "deg": 272,
+                    "clouds": 65,
+                    "rain": 1.48
+                }
+            ]
+        }
 
     def test_current_temperature_by_city_name_London_almost_equal_7_17(self):
         self.weather.data.get_current_weather_by_city_name = Mock(return_value=self.current_London_json)
@@ -204,6 +397,10 @@ class WeatherTest(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "Wrong value!"):
             self.weather.current_humidity_by_city_id(-111)
 
+    def test_week_temperature_forecast_by_city_name_London_13_52_in_temperatures(self):
+        self.weather.data.get_week_weather_by_city_name = MagicMock(return_value=self.week_London_json)
+        self.assertIn(13.52, self.weather.week_temperature_forecast_by_city_name(self.city_name_London))
+
     def tearDown(self):
         self.weather = None
 
@@ -213,6 +410,7 @@ class WeatherTest(unittest.TestCase):
         self.city_id_Mountain_View = None
         self.current_London_json = None
         self.current_Mountain_View_json = None
+        self.week_London_json = None
 
 
 if __name__ == '__main__':
