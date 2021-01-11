b1af782cb8c473232a73d3aaf78f1c4f235abc75
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:01:26 +0100

    RED test_week_pressure_forecast_by_city_id_bad_value_exception_when_invalid_city_id_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -96,11 +96,4 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_pressure_forecast_by_city_id(self, city_id):
-        try:
-            weather = self.data.get_week_weather_by_city_id(city_id)
-            pressure = []
-            for day in weather["list"]:
-                pressure.append(day["pressure"])
-            return pressure
-        except TypeError:
-            raise TypeError("Wrong type!")
+        pass
