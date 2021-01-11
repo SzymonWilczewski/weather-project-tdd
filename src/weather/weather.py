5f61d26f07fac382b54023713951187384316c18
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:11:42 +0100

    RED test_current_pressure_by_city_id_Mountain_View_equals_1023

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -33,5 +33,4 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def current_pressure_by_city_id(self, city_id):
-        weather = self.data.get_current_weather_by_city_id(city_id)
-        return weather["main"]["pressure"]
+        pass
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -154,6 +154,10 @@ class WeatherTest(unittest.TestCase):
         self.weather.data.get_current_weather_by_city_id = Mock(return_value=self.current_London_json)
         self.assertEqual(self.weather.current_pressure_by_city_id(self.city_id_London), 1012)
 
+    def test_current_pressure_by_city_id_Mountain_View_equals_1023(self):
+        self.weather.data.get_current_weather_by_city_id = Mock(return_value=self.current_Mountain_View_json)
+        self.assertEqual(self.weather.current_pressure_by_city_id(self.city_id_Mountain_View), 1023)
+
     def tearDown(self):
         self.weather = None
 
