77c0962d8223eef0614f4194b6dffdee299d50fa
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:53:24 +0100

    RED test_week_pressure_forecast_by_city_name_bad_type_exception_when_tuple_given

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -630,6 +630,11 @@ class WeatherTest(unittest.TestCase):
         self.weather.data.get_week_weather_by_city_name = MagicMock(return_value=self.week_Mountain_View_json)
         self.assertIsInstance(self.weather.week_pressure_forecast_by_city_name(self.city_name_Mountain_View), list)
 
+    def test_week_pressure_forecast_by_city_name_bad_type_exception_when_tuple_given(self):
+        self.weather.data.get_week_weather_by_city_name = MagicMock(side_effect=TypeError("Wrong type!"))
+        with self.assertRaisesRegex(TypeError, "Wrong type!"):
+            self.weather.week_pressure_forecast_by_city_name(("a", "b", "c"))
+
     def tearDown(self):
         self.weather = None
 
