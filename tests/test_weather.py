840e4d804b9788f11b9a0e034632861a42f62ecc
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 19:26:10 +0100

    RED test_week_average_temperature_by_city_id_Mountain_View_equals_10_9

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -726,6 +726,11 @@ class WeatherTest(unittest.TestCase):
         mock_method.return_value = self.week_London_json
         self.assertEqual(self.weather.week_average_temperature_by_city_id(self.city_id_London), 6.19)
 
+    @patch.object(WeatherData, 'get_week_weather_by_city_id')
+    def test_week_average_temperature_by_city_id_Mountain_View_equals_10_9(self, mock_method):
+        mock_method.return_value = self.week_Mountain_View_json
+        self.assertEqual(self.weather.week_average_temperature_by_city_id(self.city_id_Mountain_View), 10.9)
+
     def tearDown(self):
         self.weather = None
 
