9ad119dfa11070e1b72600657e795e7f3052b5ad
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:55:47 +0100

    REFACTOR test_week_pressure_forecast_by_city_name_bad_value_exception_when_invalid_city_name_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -84,4 +84,13 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_pressure_forecast_by_city_name(self, city_name):
-        raise ValueError("Wrong value!")
+        try:
+            weather = self.data.get_week_weather_by_city_name(city_name)
+            pressure = []
+            for day in weather["list"]:
+                pressure.append(day["pressure"])
+            return pressure
+        except TypeError:
+            raise TypeError("Wrong type!")
+        except ValueError:
+            raise ValueError("Wrong value!")
