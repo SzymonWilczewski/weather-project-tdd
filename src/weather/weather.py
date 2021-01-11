e3a76106128d150db5859652e6c0e09a6d63f4d9
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:22:08 +0100

    REFACTOR test_current_humidity_by_city_name_bad_type_exception_when_list_given

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -42,4 +42,8 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def current_humidity_by_city_name(self, city_name):
-        raise TypeError("Wrong type!")
+        try:
+            weather = self.data.get_current_weather_by_city_name(city_name)
+            return weather["main"]["humidity"]
+        except TypeError:
+            raise TypeError("Wrong type!")
