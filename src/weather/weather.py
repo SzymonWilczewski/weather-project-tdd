ae2a2a479a575b256f5b0152589f7900d67c44a8
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-01-11 18:44:05 +0100

    REFACTOR test_week_temperature_forecast_by_city_id_Mountain_View_10_84_in_temperatures

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -72,4 +72,8 @@ class Weather:
             raise ValueError("Wrong value!")
 
     def week_temperature_forecast_by_city_id(self, city_id):
-        return [10.84, 9.53, 7.73, 10.65, 11.21, 12.43, 13.9]
+        weather = self.data.get_week_weather_by_city_id(city_id)
+        temperature = []
+        for day in weather["list"]:
+            temperature.append(round(day["temp"]["day"] - 273.15, 2))
+        return temperature
